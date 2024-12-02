365e4589cc14d870175824c696357963f1154d40
AH-Ratul <ahratul21@gmail.com>
2024-12-02 21:30:48 +0600

    update notifier

--- observer/FlowerNotifier.js
+++ observer/FlowerNotifier.js
@@ -1,54 +1,29 @@
-import { addFlower } from "../controller/flowerController.js";
-import { db } from "../server.js";
-import { sendEmailToAll } from "../utils/email.js";
 import Subscriber from "./Subscribers.js";
 
 class FlowerNotifier {
-    notifySubscribers(flower) {
-      sendEmailToAll(flower); 
-    }
+  constructor() {
+    this.subscribers = [];
   }
 
-// class FlowerNotifier {
-//   constructor() {
-//     this.subscribers = [];
-//   }
+  // Add a subscriber
+  addSubscriber(email) {
+    const subscriber = new Subscriber(email);
+    this.subscribers.push(subscriber);
+  }
 
-//   // Load subscribers from the database
-//   loadSubscribers(callback) {
-//     const query = "SELECT email FROM subscriptions";
-//     db.query(query, (err, results) => {
-//       if (err) {
-//         console.error("Error loading subscribers:", err);
-//         return;
-//       }
-//       this.subscribers = results.map((row) => new Subscriber(row.email));
-//       console.log(
-//         "Subscribers loaded:",
-//         this.subscribers.map((sub) => sub.email)
-//       );
-//       if (callback) callback();
-//     //   addFlower({
-//     //     name: "Orchid Charm",
-//     //     price: 70,
-//     //     description: "Elegant and exotic orchids.",
-//     //     image: "/img/pngwing 7.png",
-//     //   });  
-//     });
-//   }
+  // Remove a subscriber
+  removeSubscriber(email) {
+    this.subscribers = this.subscribers.filter(
+      (subscriber) => subscriber.email !== email
+    );
+  }
 
-//   // Add a new subscriber
-//   addSubscriber(email) {
-//     const subscriber = new Subscriber(email);
-//     this.subscribers.push(subscriber);
-//   }
-
-//   // Notify all subscribers
-//   async notifySubscribers(flower) {
-//     for (const subscriber of this.subscribers) {
-//       await subscriber.update(flower);
-//     }
-//   }
-// }
+  // Notify all subscribers
+  async notifySubscribers(flower) {
+    for (const subscriber of this.subscribers) {
+      await subscriber.update(flower); // Call update for each subscriber
+    }
+  }
+}
 
 export default new FlowerNotifier();
